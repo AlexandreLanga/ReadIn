0df34a73676ff9312dd35754f9f79562e234653a
Alexandre <alelanga424@hotmail.com>
2026-02-19 02:12:57 -0300

    refactor(repositories): reorganize repositories structure by context

--- ReadIn/Repositories/LivroRepository/LivroRepository.cs
+++ ReadIn/Repositories/LivroRepository/LivroRepository.cs
@@ -1,7 +1,7 @@
 ﻿using ReadIn.Domain.Entities;
-using ReadIn.Interfaces;
+using ReadIn.Interfaces.ILivro;
 
-namespace ReadIn.Repositories
+namespace ReadIn.Repositories.LivroRepository
 {
     public class LivroRepository : ILivroRepository
     {
@@ -36,40 +36,6 @@ namespace ReadIn.Repositories
             {
                 Console.WriteLine("Ocorreu um erro ao salvar o livro! Você será encaminhado para o menu principal.");
                 Console.ReadKey(); ;
-            }
-        }
-
-        public void SalvarLeitura(Leitura leituraLivro)
-        {
-            try
-            {
-                string arquivo = $"Progresso_{leituraLivro.Nome}.txt";
-                string diretorio = "C:\\Livros\\Progressos";
-
-                if (!Directory.Exists(diretorio))
-                {
-                    Directory.CreateDirectory(diretorio);
-                }
-                if (!File.Exists(arquivo))
-                {
-                    File.Create(arquivo);
-                }
-
-                string caminho = Path.Combine(diretorio, arquivo);
-                string dados = $"{leituraLivro.Nome}\n" +
-                               $"{leituraLivro.Inicio}\n" +
-                               $"{leituraLivro.Final}\n" +
-                               $"{leituraLivro.Tempo}\n" +
-                               $"{leituraLivro.PaginasLidas}\n" +
-                               $"{leituraLivro.PontoReferencia}\n";
-                File.AppendAllText(caminho, dados);
-                Console.WriteLine($"\nProgresso salvo em {caminho}");
-                Console.ReadKey();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Ocorreu um erro ao salvar a leitura! Você será encamihado para o menu principal.");
-                Console.ReadKey();
             }
         }
 
