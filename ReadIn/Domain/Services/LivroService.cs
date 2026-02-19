9f8997ae1069f2dfe83b112eb45ad25dd48d6add
Alexandre <alelanga424@hotmail.com>
2026-02-19 02:17:02 -0300

    refactor(services): aplly dependency inversion using interfaces on services

--- ReadIn/Domain/Services/LivroService.cs
+++ ReadIn/Domain/Services/LivroService.cs
@@ -1,66 +1,20 @@
 ﻿using ReadIn.Domain.Entities;
-using ReadIn.Interfaces;
+using ReadIn.Interfaces.ILivro;
 using ReadIn.Repositories;
 
 namespace ReadIn.Domain.Services
 {
     public class LivroService : ILivroService
     {
-        private readonly LivroRepository _livroRepository;
+        private readonly ILivroRepository _livroRepository;
 
         public LivroService(
-            LivroRepository livroRepository
+            ILivroRepository livroRepository
         )
         {
             _livroRepository = livroRepository;
         }
-        public void AnotarLeitura()
-        {
-            Leitura leituraLivro = new Leitura();
-
-            string livroEscolha = "";
-            leituraLivro.Nome = EscolherLivro(livroEscolha);
-
-            Console.Clear();
-            Console.WriteLine($"| Sempre é um ótimo dia para ler {leituraLivro.Nome}!");
-            Console.WriteLine("| Assim que você confirmar, o contador será acionado e seu tempo de leitura começará!");
-            Console.WriteLine("+------------------------------------------------------------------------------------");
-            Console.WriteLine("\nPressione qualquer tecla para confirmar\n");
-            Console.ReadKey();
-            Console.Clear();
-
-            leituraLivro.Inicio = DateTime.Now;
-            Console.WriteLine("+-----------------------------------------------------+");
-            Console.WriteLine("| Seu temporizador foi iniciado!                      |");
-            Console.WriteLine("| Assim que terminar sua leitura, confirme para parar |");
-            Console.WriteLine("| o temporizador                                      |");
-            Console.WriteLine("+-----------------------------------------------------+");
-            Console.WriteLine("\nPressione qualquer tecla para confirmar\n");
-            Console.ReadKey();
-            leituraLivro.Final = DateTime.Now;
-
-            Console.Clear();
-            Console.WriteLine("+-----------------------------------------------------------------------------+");
-            Console.WriteLine("| Sua leitura chegou ao fim! Vamos ver o quanto você progrediu nessa jornada! |");
-            Console.WriteLine("+-----------------------------------------------------------------------------+");
-
-            Console.WriteLine("\n+-----------------------------------+");
-            Console.WriteLine("| Em qual página voçê parou de ler? |");
-            Console.WriteLine("+-----------------------------------+\n");
-            leituraLivro.PaginasLidas = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("\n+----------------------------------------------------------------------------------------------+");
-            Console.WriteLine("| Qual seu ponto de referência para a próxima leitura? (linha, frase, começo de paragráfo etc) |");
-            Console.WriteLine("+----------------------------------------------------------------------------------------------+\n");
-            leituraLivro.PontoReferencia = Console.ReadLine();
-
-            Console.Clear();
-            leituraLivro.Tempo = leituraLivro.Final - leituraLivro.Inicio;
-
-            AvaliarTempoDeLeitura(leituraLivro);
-            _livroRepository.SalvarLeitura(leituraLivro);
-        }
-
+        
         public void CadastrarLivro()
         {
             int opcao = 0;
@@ -115,36 +69,61 @@ namespace ReadIn.Domain.Services
 
         public void ListarLivros()
         {
-            var linhas = _livroRepository.LerLivros();
-
-            if (linhas.Length == 0)
-            {
-                Console.WriteLine("Nenhum livro cadastrado.");
-                Console.ReadKey();
-                return;
-            }
-
-            Console.Clear();
-
-            int totalLivros = linhas.Length / 6;
-
-            for (int i = 0; i < linhas.Length; i += 6)
-            {
-                Console.WriteLine($"| Id: {linhas[i]}");
-                Console.WriteLine($"| Nome: {linhas[i + 1]}");
-                Console.WriteLine($"| Autor: {linhas[i + 2]}");
-                Console.WriteLine($"| Páginas: {linhas[i + 3]}");
-                Console.WriteLine($"| Capítulos: {linhas[i + 4]}");
-                Console.WriteLine($"| Descrição: {linhas[i + 5]}");
-                Console.WriteLine("+-----------------------------------------\n");
-            }
+            var linhas = ObterLivrosCadastrados();
+            ExibirLivrosCadastrados(linhas);
+            int totalLivros = RetornarTotalDeLivros(linhas);
 
             Console.WriteLine($"Total de livros cadastrados = {totalLivros}");
             Console.WriteLine("\nPressione qualquer tecla para voltar...");
             Console.ReadKey();
         }
 
-        #region FUNÇÕES
+        #region FUNÇÕES AUXILIARES
+
+        #region REDEFINIR LIVRO
+
+        private void RedefinirInformacoesLivro(Livro livro)
+        {
+            int opcao;
+            Console.WriteLine("\n+------------------------------+");
+            Console.WriteLine("| Certo... O que deseja fazer? |");
+            Console.WriteLine("\n+------------------------------+");
+            Console.WriteLine("| 1 - Redefinir as informações |");
+            Console.WriteLine("| 2 - Voltar ao menu principal |");
+            Console.WriteLine("\n+------------------------------+");
+
+            opcao = int.Parse(Console.ReadLine());
+
+            if (opcao == 1)
+            {
+                Console.Clear();
+
+                Console.WriteLine("\n+-----------------------------------+");
+                Console.WriteLine("| Qual informação deseja redefinir? |");
+                Console.WriteLine("\n+-----------------------------------+");
+                Console.WriteLine("| 1 - Id do livro                   |");
+                Console.WriteLine("| 2 - Nome do livro                 |");
+                Console.WriteLine("| 3 - Autor do livro                |");
+                Console.WriteLine("| 4 - Páginas do livro              |");
+                Console.WriteLine("| 5 - Capítulos                     |");
+                Console.WriteLine("| 6 - Descrição                     |");
+                Console.WriteLine("\n+-----------------------------------+");
+
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1: livro.LivroId = RedefinirId(livro.LivroId); break;
+                    case 2: livro.Nome = RedefinirNome(livro.Nome ?? ""); break;
+                    case 3: livro.Autor = RedefinirAutor(livro.Autor ?? ""); break;
+                    case 4: livro.Paginas = RedefinirPaginas(livro.Paginas); break;
+                    case 5: livro.Capitulos = RedefinirCapitulos(livro.Capitulos); break;
+                    case 6: livro.Descricao = RedefinirDescricao(livro.Descricao ?? ""); break;
+                }
+
+                ApresentarLivroParaCadastro(livro);
+            }
+        }
 
         private static int RedefinirId(int livroId)
         {
@@ -238,73 +217,7 @@ namespace ReadIn.Domain.Services
             }
         }
 
-        private string EscolherLivro(string livro)
-        {
-            Console.Clear();
-
-            int achou = 0, contador = 1;
-            string opcao = "";
-
-            Console.WriteLine("+------------------------------------------------------------------------+");
-            Console.WriteLine("| Nunca é uma hora ruim para ler e adquirir vastos conhecimentos, certo? |");
-            Console.WriteLine("| Qual livro vamos ler agora?                                            |");
-            Console.WriteLine("+------------------------------------------------------------------------+\n");
-
-
-            string[] livros = _livroRepository.LerLivros();
-
-            foreach (string linha in livros)
-            {
-                if (contador == 1)
-                {
-                    livro = linha + " - ";
-                    contador++;
-                }
-                else
-                {
-                    if (contador == 2)
-                    {
-                        livro += linha;
-                        Console.WriteLine($"| {livro}");
-                        Console.WriteLine("-------------------------------------------------------------------------");
-                        contador++;
-                    }
-                    else
-                    {
-                        if (contador == 6)
-                        {
-                            contador = 1;
-                        }
-                        else
-                        {
-                            contador++;
-                        }
-                    }
-                }
-            }
-
-            Console.WriteLine("\n+----------------------+");
-            Console.WriteLine("| Digite o Id do livro:|");
-            Console.WriteLine("+----------------------+\n");
-            opcao = (Console.ReadLine());
-
-            foreach (string linha in livros)
-            {
-                if (opcao == linha)
-                {
-                    achou = 1;
-                }
-                else
-                {
-                    if (achou == 1)
-                    {
-                        livro = linha;
-                        break;
-                    }
-                }
-            }
-            return livro;
-        }
+        #endregion
 
         private void ApresentarLivroParaCadastro(Livro livro)
         {
@@ -337,97 +250,42 @@ namespace ReadIn.Domain.Services
 
             else
             {
-                RedefinirInformacoes(livro);
+                RedefinirInformacoesLivro(livro);
             }
         }
 
-        private void RedefinirInformacoes(Livro livro)
+        public string[] ObterLivrosCadastrados()
         {
-            int opcao;
-            Console.WriteLine("\n+------------------------------+");
-            Console.WriteLine("| Certo... O que deseja fazer? |");
-            Console.WriteLine("\n+------------------------------+");
-            Console.WriteLine("| 1 - Redefinir as informações |");
-            Console.WriteLine("| 2 - Voltar ao menu principal |");
-            Console.WriteLine("\n+------------------------------+");
+            return _livroRepository.LerLivros();
+        }
 
-            opcao = int.Parse(Console.ReadLine());
-
-            if (opcao == 1)
+        public void ExibirLivrosCadastrados(string[] linhas)
+        {
+            if (linhas.Length == 0)
             {
-                Console.Clear();
+                Console.WriteLine("Nenhum livro cadastrado.");
+                Console.ReadKey();
+                return;
+            }
 
-                Console.WriteLine("\n+-----------------------------------+");
-                Console.WriteLine("| Qual informação deseja redefinir? |");
-                Console.WriteLine("\n+-----------------------------------+");
-                Console.WriteLine("| 1 - Id do livro                   |");
-                Console.WriteLine("| 2 - Nome do livro                 |");
-                Console.WriteLine("| 3 - Autor do livro                |");
-                Console.WriteLine("| 4 - Páginas do livro              |");
-                Console.WriteLine("| 5 - Capítulos                     |");
-                Console.WriteLine("| 6 - Descrição                     |");
-                Console.WriteLine("\n+-----------------------------------+");
+            Console.Clear();
 
-                opcao = int.Parse(Console.ReadLine());
-
-                switch (opcao)
-                {
-                    case 1: livro.LivroId = RedefinirId(livro.LivroId); break;
-                    case 2: livro.Nome = RedefinirNome(livro.Nome ?? ""); break;
-                    case 3: livro.Autor = RedefinirAutor(livro.Autor ?? ""); break;
-                    case 4: livro.Paginas = RedefinirPaginas(livro.Paginas); break;
-                    case 5: livro.Capitulos = RedefinirCapitulos(livro.Capitulos); break;
-                    case 6: livro.Descricao = RedefinirDescricao(livro.Descricao ?? ""); break;
-                }
-
-                ApresentarLivroParaCadastro(livro);
+            for (int i = 0; i < linhas.Length; i += 6)
+            {
+                Console.WriteLine($"| Id: {linhas[i]}");
+                Console.WriteLine($"| Nome: {linhas[i + 1]}");
+                Console.WriteLine($"| Autor: {linhas[i + 2]}");
+                Console.WriteLine($"| Páginas: {linhas[i + 3]}");
+                Console.WriteLine($"| Capítulos: {linhas[i + 4]}");
+                Console.WriteLine($"| Descrição: {linhas[i + 5]}");
+                Console.WriteLine("+-----------------------------------------\n");
             }
         }
 
-        private void AvaliarTempoDeLeitura(Leitura leituraLivro)
+        private int RetornarTotalDeLivros(string[] linhas)
         {
-            Console.WriteLine("+-----------------------------------+");
-            Console.WriteLine("| Veja a progressão da sua leitura: |");
-            Console.WriteLine("+-----------------------------------+-------------------------------------------------------------------------------");
-            Console.WriteLine($"| Livro lido: {leituraLivro.Nome}");
-            Console.WriteLine("+-------------------------------------------------------------------------------------------------------------------");
-            Console.WriteLine($"| Horário de início da leitura: {leituraLivro.Inicio}");
-            Console.WriteLine("+-------------------------------------------------------------------------------------------------------------------");
-            Console.WriteLine($"| Horário de finalização da leitura: {leituraLivro.Final}");
-            Console.WriteLine("+-------------------------------------------------------------------------------------------------------------------");
-            Console.WriteLine($"| Tempo em leitura: {leituraLivro.Tempo}");
-            Console.WriteLine("+-------------------------------------------------------------------------------------------------------------------");
-            Console.WriteLine($"| Páginas lidas: {leituraLivro.PaginasLidas}");
-            Console.WriteLine("+-------------------------------------------------------------------------------------------------------------------");
-            Console.WriteLine($"| Último ponto de referência: {leituraLivro.PontoReferencia}");
-            Console.WriteLine("+-------------------------------------------------------------------------------------------------------------------");
-
-            DateTime leuPadrao, leuMediano, leuAvancado;
-            DateTime.TryParse("00:15", out leuPadrao);
-            DateTime.TryParse("00:30", out leuMediano);
-            DateTime.TryParse("00:59", out leuAvancado);
-
-            if (leituraLivro.Tempo.Minutes <= leuPadrao.Minute)
-            {
-                Console.WriteLine("\nAquela leitura rapidinha no tempo vago, quem nunca?");
-            }
-
-            if (leituraLivro.Tempo.Minutes > leuPadrao.Minute && leituraLivro.Tempo.Minutes <= leuMediano.Minute)
-            {
-                Console.WriteLine("\nAquela doce e alegre leitura simples para não perder o costume!");
-            }
-
-            if (leituraLivro.Tempo.Minutes >= leuMediano.Minute && leituraLivro.Tempo.Minutes < leuAvancado.Minute)
-            {
-                Console.WriteLine("\nLer por mais de meia hora definitivamente aumenta sua criatividade!");
-            }
-
-            if (leituraLivro.Tempo.Minutes >= leuAvancado.Minute)
-            {
-                Console.WriteLine("\nUau! Você leu por uma hora ou mais! Você definitivamente leu bastante, se orgulhe disso!");
-            }
-
-            Console.ReadKey();
+            int totalLivros = linhas.Length / 6;
+            return totalLivros;
         }
 
         #endregion
